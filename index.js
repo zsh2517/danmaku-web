c71a6994d180594ad914469b238c46b34de7f7fa
Billchenchina <billchenchina2001@gmail.com>
2019-11-20 19:19:18 +0800

    bug fix

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function redirect() {
     let stuid = document.getElementById("stuid").value;
     if(stuid.trim() == "") {
         alert("请输入学号再试试~");
-        return;
+        // return;
     }
     if(!docCookies.hasItem("stuid")) {
-        docCookies.setItem("stuid", toString(stuid));
+        docCookies.setItem("stuid", stuid);
     }
     location.href = "send.html";
 }
