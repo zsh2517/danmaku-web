403667083020c46803b1a1e640b1d889ce4dc5b8
billchenchina <billchenchina2001@gmail.com>
2019-12-10 18:27:28 +0800

    Bugfix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function redirect() {
     let stuid = document.getElementById("stuid").value;
     if(stuid.trim() == "") {
         alert("请输入学号再试试~");
-        // return;
+        return;
     }
     if(!docCookies.hasItem("stuid")) {
         docCookies.setItem("stuid", stuid);
